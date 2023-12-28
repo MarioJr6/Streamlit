2a6f3152d43060256cb1cab041751aaa3c79cfdf
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2023-12-28 15:13:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import streamlit as st
 
-dash_1 = st.container()
-with dash_1:
-    st.markdown("<h2 style='text-align: center;'>Monitoramento Ambiental de SARS-CoV-2</h2>", unsafe_allow_html=True)
-    st.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/Logo%20CEVS.png?raw=true', width=200)
-        
-    st.write("")
+st.set_page_config(
+    page_title="Painel de Monitoramento Ambiental de SARS-CoV-2",
+    page_icon="	:snake:",
+    layout="wide",
+    initial_sidebar_state='collapsed'
+)
+col1, col2, col3 = st.columns([1,4,1])
+col1.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/Logo%20CEVS.png?raw=true', width=200)
+col2.title('Painel de Monitoramento Ambiental de SARS-CoV-2')
+col3.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/Logo%20Estado.png?raw=true', width=300)
 
 
