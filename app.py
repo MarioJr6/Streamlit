569d9f6af7d73030f1acef33e6f1082657b8bf1b
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2023-12-28 15:34:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,4 @@ df_esgoto['carga_viral_n1'] = df_esgoto['carga_viral_n1'].astype(float)
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 
 container_2 = st.container() 
-with container_2: 
-    #coluna1, coluna2, colun3 = st.columns(3)
-    #coluna1.metric('teste')
+with container_2:
