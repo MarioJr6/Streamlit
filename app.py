cc2251319bb42e1b574173010fdea3b5989c06ba
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2023-12-29 15:51:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ with container_2:
 
     # Atualize o layout do gráfico para ocupar toda a largura disponível
     fig.update_layout(
-        width=1500,  # Definir uma largura fixa
+        width=1800,  # Definir uma largura fixa
         height=500,  # Definir uma altura fixa
     )
     st.plotly_chart(fig)
