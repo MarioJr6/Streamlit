5d5ce857c00e2239143ebca21357c1d066fc74aa
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2023-12-29 14:05:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,13 @@ with container_2:
     col3.metric(label="Carga Viral (CG/L) de SARS-CoV-2 na ultima amostra de esgoto", 
                 value=df_esgoto_filtrado['carga_viral_n1'].iloc[-1])
 
+    col4.metric(label="Data da última análise ambiental", 
+                value=df_esgoto_filtrado['carga_viral_n1'].iloc[-1])
+
+
+    data_esgoto
+    
+
     
  
 
