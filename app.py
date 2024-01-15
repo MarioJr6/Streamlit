8c94b30c4e2bd0738f80388539e0ddf5116f87ef
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-15 09:40:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import streamlit as st
 import pandas as pd
 import requests
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import streamlit as st
 
-#
+from plotly.subplots import make_subplots
+from streamlit_extras.metric_cards import style_metric_cards
+
 st.set_page_config(
     page_title="Painel de Monitoramento Ambiental de SARS-CoV-2",
     page_icon="	:snake:",
