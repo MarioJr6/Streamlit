82e430e77823cdf11bac8df9169647cdb45560dd
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-16 09:10:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ with container_2:
 
     container_3 = st.container()
     with container_3: 
-        col1, col2, col3, col4 = st.columns([1,1,1,1])
+        col1, col2, col3, col4 = st.columns([1,2,1,1])
         col1.write("Análises ambintais realizadas pelo Laboratório Virologia do ICBS UFRGS")
         col1.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/ufrgs.png?raw=true', width=200)
 
