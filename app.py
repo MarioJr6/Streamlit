655bab7616ac60bbceeaaf63243b680aab1aa834
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-16 09:24:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ with container_2:
         tabela = tabela.drop('Data de coleta', axis=1)
 
         
-        #teste = tabela.groupby[('Município','Mês')].sum()
+        #teste = tabela.groupby[('Local de coleta','Mês')].sum()
         
         
 
