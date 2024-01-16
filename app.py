b8609fb49a3cc573ea7b572450f5c857a696e2c3
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-16 09:32:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ with container_2:
         tabela = tabela.drop('Data de coleta', axis=1)
 
         
-        matriz = tabela.groupby('Mês').sum()
+        matriz = tabela.groupby('Mês').sum().drop_index()
         
 
         
