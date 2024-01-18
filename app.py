fdb6e080edc86822ba8413a523f9613f2317dce2
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-18 14:14:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ with container_2:
 
     # Atualize o layout do gráfico para ocupar toda a largura disponível
     fig.update_layout(
-        title="Carga viral no esgoto bruto e Casos de COVID 19 em Porto Alegre",
+        #title="Carga viral no esgoto bruto e Casos de COVID 19 em Porto Alegre",
         width=1800,  # Definir uma largura fixa
         height=560,  # Definir uma altura fixa
     )
