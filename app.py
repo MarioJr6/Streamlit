f2162370b4dcc769c6086de240e943e51cde0e6f
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-19 13:42:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -109,14 +109,19 @@ with container_2:
         
     matriz['Variação absoluta'] = matriz['carga_viral_n1'].diff()
     matriz['Variação absoluta'].fillna("Sem dados", inplace= True)
-    
+
     matriz['Variação em porcentagem'] = matriz['carga_viral_n1'].pct_change() * 100
     matriz['Variação em porcentagem'].fillna("Sem dados", inplace= True)
 
     matriz = matriz.rename(columns={'carga_viral_n1':'Média da carga viral mensal'})
+
+    tipo_dados = matriz.dtypes
+    
     col4.table(matriz.style.set_table_styles(
     [
         dict(selector="thead th", props=[("background-color", "#3498db"), ("color", "white")]),
         dict(selector="tbody td", props=[("border", "1px solid #dddddd")]),
     ]
     ))
+
+    col4.write(tipo_dados)
