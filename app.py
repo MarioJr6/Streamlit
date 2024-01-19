1d0224355de6f106a0017248603a8dad58ec2240
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-19 11:42:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ fig = make_subplots(specs=[[{"secondary_y": True}]])
 container_2 = st.container() 
 with container_2:
     col1, col2, col3, col4 = st.columns([1,1,1,1])
-     col1.markdown(
+    col1.markdown(
         """
         <style>
             div[data-baseweb="select"] {
