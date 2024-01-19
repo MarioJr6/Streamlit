748dfbc03286146a6caec391361b60a9134e6de3
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-19 14:01:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -108,10 +108,10 @@ with container_2:
     matriz = tabela.groupby('Mês').mean().reset_index()
         
     matriz['Variação absoluta'] = matriz['carga_viral_n1'].diff()
-    matriz['Variação absoluta'] = pd.to_numeric(matriz['Variação absoluta'], errors='coerce').astype('Int64')
+    #matriz['Variação absoluta'] = pd.to_numeric(matriz['Variação absoluta'], errors='coerce').astype('Int64')
 
     matriz['Variação em porcentagem'] = matriz['carga_viral_n1'].pct_change() * 100
-    matriz['Variação em porcentagem'] = pd.to_numeric(matriz['Variação em porcentagem'], errors='coerce').astype('Int64')
+    #matriz['Variação em porcentagem'] = pd.to_numeric(matriz['Variação em porcentagem'], errors='coerce').astype('Int64')
 
     
     matriz['Variação absoluta'].fillna("Sem dados", inplace= True)
