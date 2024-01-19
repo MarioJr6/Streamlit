54e19d21920238f8b13ef4a822ab1881a3d63272
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-19 11:25:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,8 @@ with container_2:
     col4.write("")
     col4.write("")
     
-    col4.write("Análises ambintais realizadas pelo Laboratório de Virologia do ICBS UFRGS")
+    col4.write("Análises ambientais realizadas pelo Laboratório de Virologia do ICBS UFRGS")
+    col4.text('Município selecionado: {}'.format(muni))
 
     col4.write("")
     col4.write("")
