3601267b19cdd8fc188ea8f1c0ce26a0f730c1ec
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-19 10:34:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ with container_2:
     
     col1.plotly_chart(fig)
 
-    col3.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/ufrgs.png?raw=true', width=200)
+    col4.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/ufrgs.png?raw=true', width=200)
 
     container_3 = st.container()
     with container_3: 
