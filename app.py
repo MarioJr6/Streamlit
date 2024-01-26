bb1844f149118a0e03f6410bfac5cf28393ed545
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-26 14:27:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ with container_2:
     lista_de_datas = df_esgoto2['Data de coleta'].tolist()
 
     for i in lista_de_datas: 
-        col1.write(type(i))
+        st.write(f"{i}: {type(i)}")
 
         
     # Métricas para as informações desejadas no painel, distribuidas nas colunas estabelecidas
