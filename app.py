ab5778759f82ac8fa534d621907e2d87e475cb97
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-26 14:40:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -98,12 +98,6 @@ with container_2:
     # Criando uma lista com as datas de coleta
     lista = df_esgoto2['Data de coleta'].tolist()
     
-    for data in lista:
-        lista_datas = []
-        #partes = data.split('-')
-        #data_ordenada = '-'.join(reversed(partes))
-        lista_datas.append(data)
-
     # Métricas para as informações desejadas no painel, distribuidas nas colunas estabelecidas
     col2.metric(label = "Casos de COVID 19 confirmados nos últimos 7 dias", 
                 value = df_casos_filtrado.tail(7).sum())
@@ -209,4 +203,4 @@ with container_2:
     ]
     ))
 
-    lista_datas
+    lista
