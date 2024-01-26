af02049e3061004b9daa731812b21d9a29a4e36e
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-01-26 14:42:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -97,9 +97,9 @@ with container_2:
     df_esgoto2['Data de coleta'] = df_esgoto2['Data de coleta'].astype(str)
     # Criando uma lista com as datas de coleta
     lista = df_esgoto2['Data de coleta'].tolist()
-
+    
+    teste = []
     for i in lista: 
-        teste = []
         teste.append(i)
     
     
