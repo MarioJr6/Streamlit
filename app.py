ef86c9a4a528e8ce91c75508823f186172879cbd
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-02-05 10:07:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -224,3 +224,6 @@ with container_2:
     # URL que você deseja redirecionar
     painel = 'https://app.powerbi.com/view?r=eyJrIjoiNDcxNGU5YTItZTU5Mi00MDZkLTljNTMtZTBmZDk2NTAyNzNkIiwidCI6IjE1ZGNkOTA5LThkYzAtNDBlOS1hMWU1LWNlY2IwNTNjZGQxYSJ9'
     st.link_button("Voltar para o painel", painel)
+
+    tabela
+    
