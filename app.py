0596ab900cf85b3ab7785c318eeb157f591275ec
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-02-05 10:00:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -45,10 +45,10 @@ def fetch_and_clean_data(url):
     return grouped
 
 # Dicionário dos meses
-meses = { 1: 'Janeiro', 2: 'Fevereiro', 3: 'Março',
-         4: 'Abril', 5: 'Maio', 6: 'Junho',
-         7: 'Julho', 8: 'Agosto', 9: 'Setembro',
-         10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'}
+meses = { 0: 'Janeiro', 1: 'Fevereiro', 2: 'Março',
+         3: 'Abril', 4: 'Maio', 5: 'Junho',
+         6: 'Julho', 7: 'Agosto', 8: 'Setembro',
+         9: 'Outubro', 10: 'Novembro', 11: 'Dezembro'}
 
 # Realizando a leitura dos dados para utilizar no painel
 df_casos = fetch_and_clean_data('https://ti.saude.rs.gov.br/covid19/download?2023')
