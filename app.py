e11b75893f00563cec5443606da761d8497871b6
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-03-04 10:05:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ with container_1:
     # Adicionando as imagens ao painel e o meu título desejado, centralizando dele no meio do painel
     col1.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/Logo%20CEVS.png?raw=true', width=200)
     col2.markdown("<h2 style='text-align: center;'>Painel de Monitoramento Ambiental de SARS-CoV-2</h2>", unsafe_allow_html=True)
-    col2.write(f"Atualizado em {get_current_time()}")
+    col2.write(f"<h2 style='text-align: center;'>Atualizado em {get_current_time()}</h2>")
     col3.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/Logo%20Estado.png?raw=true', width=300)
   
 
