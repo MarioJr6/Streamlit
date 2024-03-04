fba3471ae477166ab20dc61ed45d86143b68d25b
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-03-04 11:29:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ municipio = ['CAPÃO DA CANOA', 'CAXIAS DO SUL', 'PASSO FUNDO', 'SANTA MARIA', '
 df_esgoto.dtypes
 
 # Formatando para o tipo data
-#df_esgoto['Data de coleta'] = pd.to_datetime(df_esgoto['Data de coleta'], format='%d/%m/%Y')
+df_esgoto['Data de coleta'] = pd.to_datetime(df_esgoto['Data de coleta'], format='%d/%m/%Y')
 # Filtrando para o período selecionado
 #df_esgoto = df_esgoto[df_esgoto['Data de coleta']>='2023-01-01']
 # Transformando a a coluna carga viral para o tipo float
