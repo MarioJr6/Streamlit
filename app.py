280e48a71c0ec583b7d68d4d167571804a3fa935
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-03-04 11:30:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ df_esgoto.dtypes
 df_esgoto['Data de coleta'] = pd.to_datetime(df_esgoto['Data de coleta'], format='%d/%m/%Y')
 df_esgoto.dtypes
 # Filtrando para o período selecionado
-#df_esgoto = df_esgoto[df_esgoto['Data de coleta']>='2023-01-01']
+df_esgoto = df_esgoto[df_esgoto['Data de coleta']>='2023-01-01']
 # Transformando a a coluna carga viral para o tipo float
-#df_esgoto['carga_viral_n1'] = df_esgoto['carga_viral_n1'].astype(float)
+df_esgoto['carga_viral_n1'] = df_esgoto['carga_viral_n1'].astype(float)
 
 
