b40f297b085871bf8f2f417b530ee708cdf8d9d9
MarioJr6 <154622586+MarioJr6@users.noreply.github.com>
2024-03-04 11:27:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,28 @@ with container_1:
     col2.markdown("<h2 style='text-align: center;'>Painel de Monitoramento Ambiental de SARS-CoV-2</h2>", unsafe_allow_html=True)
     col2.markdown(f"<p style='text-align: center;'>Atualizado em {data_atualizacao()}</p>", unsafe_allow_html=True)
     col3.image('https://github.com/MarioJr6/MonitoramentoAmbiental/blob/main/Logo%20Estado.png?raw=true', width=300)
+
+# Função para tratamento dos meus dados contendo os casos de covid
+@st.cache_data
+def funcao_covid(url):
+    df_casos = pd.read_csv(url, encoding="UTF-8", sep=";")
+    df_casos['DATA_SINTOMAS']=pd.to_datetime(df_casos['DATA_SINTOMAS'], format='%d/%m/%Y')
+    df_casos['DATA_CONFIRMACAO']=pd.to_datetime(df_casos['DATA_CONFIRMACAO'], format='%d/%m/%Y')
+
+  # Agrupando os dados de forma que eu tenha todas as datas do ano
+    grouped = pd.pivot_table(data=df_casos, index='DATA_SINTOMAS', columns='MUNICIPIO', values='CRITERIO', aggfunc='count').fillna(0).reset_index()
+    colunas = ['DATA_SINTOMAS', 'CAPÃO DA CANOA', 'CAXIAS DO SUL', 'PASSO FUNDO',
+           'SANTA MARIA', 'SANTA ROSA', 'TORRES']
+    grouped = grouped[colunas]
+    
+    return grouped
+
+# Dicionário dos meses
+meses = { 1: 'Janeiro', 2: 'Fevereiro', 3: 'Março',
+          4: 'Abril', 5: 'Maio', 6: 'Junho',
+          7: 'Julho', 8: 'Agosto', 9: 'Setembro',
+          10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'}
+
+# Realizando a leitura dos dados para utilizar no painel
+df_casos = funcao_covid('https://ti.saude.rs.gov.br/covid19/download?2023')
+df_esgoto = pd.read_table('https://docs.google.com/spreadsheets/d/e/2PACX-1vTZfjxdY8_x5WNd9_NE3QQPeche-dMdY5KdvNpq8H4W-lmUTidwrKpV0uLzLtihV7UAPIl68WvugMsN/pub?gid=0&single=true&output=tsv')
